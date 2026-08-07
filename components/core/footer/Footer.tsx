216ee163bb49358bf0239e39c832af3b9c28863e
HagenSV <git@hagensv.dev>
2026-08-07 08:14:52 -0400

    fix footer links

--- components/core/footer/Footer.tsx
+++ components/core/footer/Footer.tsx
@@ -4,20 +4,20 @@ import FooterSection from "./FooterSection";
 
 export default function Footer(){
     return (
-        <footer className="mt-20 pt-10 border-t-1 border-zinc-300 bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-900">
+        <footer className="mt-10 pt-10 border-t-1 border-zinc-300 bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-900">
             <div className="grid grid-cols-12">
                 <div className="col-span-4 md:col-span-7"></div>
                 <div className="col-span-4 md:col-span-2">
                     <FooterSection title="Generators">
                         { games.map( game => (
-                            <p><StyledLink href={game.baseUrl}>{game.name}</StyledLink></p>
+                            <StyledLink href={game.baseUrl} key={game.baseUrl}>{game.name}</StyledLink>
                         )) }
                     </FooterSection>
                 </div>
                 <div className="col-span-4 md:col-span-3">
-                    <FooterSection title="Company">
-                        <p><StyledLink href="/about">About</StyledLink></p>
-                        <p><StyledLink href="/company/privacy">Privacy Policy</StyledLink></p>    
+                    <FooterSection title="Boring Stuff">
+                        <p><StyledLink href="/company">About</StyledLink></p>
+                        <p><StyledLink href="/company/privacy-policy">Privacy Policy</StyledLink></p>    
                     </FooterSection>
                 </div>
             </div>
