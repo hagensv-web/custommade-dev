fb006e44f5425cd6f756fb595ddd562a042be1a6
HagenSV <git@hagensv.dev>
2026-08-07 08:04:16 -0400

    separate games into current and coming soon

--- data/games.ts
+++ data/games.ts
@@ -5,11 +5,14 @@ interface Game {
 }
 
 export const games: Game[] = [
-      {
+  {
     name: "Bingo!",
     baseUrl: "/bingo",
     description: "Create your own custom bingo cards for classrooms, family gatherings, or parties. Enter the items you want to appear on the card and generate as many randomized cards as you want. Print them to share with a group, or send them to your friends and play in your browser."
   },
+]
+
+export const comingSoon: Game[] = [
   {
     name: "Hidden Phrase",
     baseUrl: "/word-guess",
@@ -22,7 +25,17 @@ export const games: Game[] = [
   },
   {
     name: "Trivia Categories",
-    baseUrl: "category-trivia",
+    baseUrl: "/category-trivia",
     description: "Create your own Jepoardy-like trivia game. Create your own categories, point scaling, bonuses and more! When your game is ready, share your screen and you're ready to host."
+  },
+  {
+    name: "Crossword",
+    baseUrl: "/crossword",
+    description: "Create your own crossword or words fit game. Create your own list of words and provide clues for each one. Or for a different challenge, provide no clues and instead have players try to make the words fit into the puzzle"
+  },
+  {
+    name: "Word Search",
+    baseUrl: "/word-search",
+    description: "Your classic word search, create your own word list and customize rules for "
   }
 ]
