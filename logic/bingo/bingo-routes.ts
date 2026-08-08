b28f48abe7739f13598b72035f5c73535dd6cc43
HagenSV <git@hagensv.dev>
2026-08-07 23:38:28 -0400

    fixed bingo routes

--- logic/bingo/bingo-routes.ts
+++ logic/bingo/bingo-routes.ts
@@ -10,22 +10,22 @@ export class BingoRoutes {
     }
 
     static edit(card: BingoCardManage){
-        return `${BINGO_BASE}/edit/?card=${card.getData().id}`
+        return `${BINGO_BASE}/edit?card=${card.getData().id}`
     }
 
     static preview(card: BingoCardManage){
-        return `${BINGO_BASE}/preview/?card=${card.getData().id}`
+        return `${BINGO_BASE}/preview?card=${card.getData().id}`
     }
 
     static share(card: BingoCardManage){
-        return `${BINGO_BASE}/share/?data=${card.export()}`
+        return `${BINGO_BASE}/share?data=${card.export()}`
     }
 
     static play(game: BingoGameManage){
-        return `${BINGO_BASE}/play/?game=${game.getData().id}`
+        return `${BINGO_BASE}/play?game=${game.getData().id}`
     }
 
     static create(){
-        return `${BINGO_BASE}/create`
+        return `${BINGO_BASE}/edit`
     }
 }
